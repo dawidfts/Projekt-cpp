3a0ab63db523fe97fd145ab88a26d1fdba56ed4d
Dawid <dawid.fts@gmail.com>
2018-03-09 14:56:57 +0100

    update
    update

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "projekt zaczety!" << endl;
+    cout << "hellow world!" << endl;
+    cout << "ciekawe czy juz dzi³a to na git?" << endl;
+    return 0;
+}
