a3a8cb7f9c1b5690539c6315966de6d17e85935a
Dawid <dawid.fts@gmail.com>
2018-03-13 22:16:47 +0100

    no coś tam dodałem

--- main.cpp
+++ main.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+void wstep();
+void sortowanie();
+void autorzy();
+void exite();
+
 int main()
 {
-    cout << "projekt zaczety!" << endl;
-    cout << "hellow world!" << endl;
-    cout << "ciekawe czy juz dzi³a to na git?" << endl;
+    int chois;
+    while(true){
+        cout << "***** Sortowanie Liczb caÅ‚kowitych poprzez wybieranie i przez kopcowanie  *****" << endl;
+        cout << "1. Wstep" << endl;
+        cout << "2. Sortowanie" << endl;
+        cout << "3. Autorzy" << endl;
+        cout << "4. Wyjscie" << endl;
+
+        cin>>chois;
+
+        switch(chois){
+            case 1: wstep();        break;
+            case 2: sortowanie();   break;
+            case 3: autorzy();      break;
+            case 4: exite();        break;
+            default: cout<<"Nie poprawna komenda"<< endl;
+        }
+    }
+
     return 0;
+}
+
+void wstep()
+{
+
+}
+
+void sortowanie()
+{
+
+}
+
+void autorzy()
+{
+
+}
+
+void exite()
+{
+    exit(0);
 }
