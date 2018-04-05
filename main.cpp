d0f910e663fe004915c2cf3dc147dc14feb54909
Kasiaak <35401505+Kasiaak@users.noreply.github.com>
2018-04-06 01:08:36 +0200

    update menu+file with selection sort
    menu must be fixed and upgraded- elementary style of writing and no functions for selection sort

--- main.cpp
+++ main.cpp
@@ -1,12 +1,79 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
 
-using namespace std;
+#include "menu_functions.cpp"
 
-int main()
+int print_headers(void)
 {
-    cout << "projekt zaczety!" << endl;
-    cout << "hellow world!" << endl;
-    cout << "ciekawe czy juz dzi³a to na git?" << endl;
-    cout << " sukces xD czas zaczac prace " << endl;
+    int user_choice;
+    cout << "Menu\nAby przejsc do wybranego punktu z wymienionych ponizej wpisz cyfre przypisana do opisu operacji.\n"
+         "1. Czesc teoretyczna\n2. Symulacje\n3. Autorzy\n4. Wyjscie\n\nTwoj wybor: ";
+    cin >> user_choice;
+    return user_choice;
+}
+
+void read_theory()
+{
+    fstream theory;
+    theory.open("theory.txt", ios :: in);
+    if(theory.good() == true)
+    {
+        cout << "dostep do pliku\n";
+        string text;
+        getline(theory,text);
+        cout << text;
+    }
+    else
+    {
+        cout << "blad dostepu do pliku";
+    }
+
+
+}
+
+int operation(int user_choice)
+{
+    bool check = true;
+    while( check == true)
+    {
+        switch(user_choice)
+        {
+        case 1:
+            read_theory();
+            check = false;
+            break;
+        case 2:
+            cout << "2";
+            check = false;
+            break;
+        case 3:
+            cout << "3";
+            check = false;
+            break;
+        case 4:
+            return 0;
+            check = false;
+            break;
+        default:
+            do
+            {
+                cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
+                cin >> user_choice;
+                if(user_choice < 5)
+                {
+                    check = true;
+                }
+            }
+            while(user_choice > 4);
+        }
+    }
+}
+
+
+
+int main(void)
+{
+    operation(print_headers());
     return 0;
 }
