c760ca2f456edaa6672da651be0970552f512125
Dawid <dawid.fts@gmail.com>
2018-05-22 23:29:25 +0200

    pomiar czasu + pierdoły
    Takie tam lekkie usprawnienia

--- main.cpp
+++ main.cpp
@@ -4,20 +4,28 @@
 
 #include "menu_functions.cpp"
 
-void insertionSort(int arr[], int n)
+void insertionSort(long int arr[], int n)
 {
-   int key, j, counter = 0;
-   for (int i = 1; i < n; i++){
-       key = arr[i];
-       j = i-1;
-       while (j >= 0 && arr[j] > key){
-           arr[j+1] = arr[j];
-           j = j-1;
-           counter++;
-       }
-       arr[j+1] = key;
-   }
-   cout<<"\nWykonano "<<counter<<" operacji"<<endl;
+    std::clock_t start;
+    double duration;
+
+    start = std::clock();
+    long long int counter = 0;
+    int key, j;
+    for (int i = 1; i < n; i++){
+        key = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j-1;
+            counter++;
+        }
+        arr[j+1] = key;
+    }
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+    std::cout<<"printf: "<< duration <<'\n';
+    cout<<"\nWykonano "<<counter<<" operacji"<<endl;
 }
 
 /*void heapSort(int d[], int n)
@@ -34,7 +42,7 @@ void insertionSort(int arr[], int n)
     }
 }*/
 
-int *fill_array(int arr[], int n)
+long int *fill_array(long int arr[], int n)
 {
     srand(time(0));
     for (int i = 0; i < n; i++){
@@ -56,6 +64,78 @@ void printArray(int arr[], int n)
         cout << arr[i] << endl;
 }
 
+int main(void)
+{
+    menu();
+    return 0;
+}
+
+int menu()
+{
+    bool check = true;
+    int user_choice;
+    cout << "\t\t\t\t| Menu |\n\nAby przejsc do wybranego punktu z wymienionych ponizej wpisz cyfre przypisana do opisu operacji.\n"
+         "\n1. Czesc teoretyczna\n2. Symulacje\n3. Autorzy\n4. Wyjscie\n\nTwoj wybor: ";
+    do{
+        cin >> user_choice;
+        switch(user_choice){
+            case 1:
+                read_theory();  check = false;break;
+            case 2:
+                menu_sortowanie();   check = false;  break;
+            case 3:
+                cout << "3";    check = false;  break;
+            case 4:
+                return 0;   check = false;  break;
+            default:
+                    cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
+    }
+    }while( check == true);
+    return 0;
+}
+
+int menu_sortowanie()
+{
+    system("cls");
+    cout <<"\t\t\t\t| Menu |\n\n1. Klasyczne sortowanie \n2. \n3. \n4. \n\nTwoj wybor: ";
+    int user_choice;
+    bool check = true;
+    do{
+        cin >> user_choice;
+        switch(user_choice){
+            case 1:
+                classic_sort();  check = false;  break;
+            case 2:
+                cout << "2";    check = false;  break;
+            case 3:
+                cout << "3";    check = false;  break;
+            case 4:
+                return 0;   check = false;  break;
+            default:
+                    cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
+    }
+    }while( check == true);
+    return 0;
+}
+
+int classic_sort()
+{
+    system("cls");
+    cout << "W klasycznym sortowaniu zostana wygenerowane tablice o roznych pojemnosciach\n"
+        "liczbami losowymi i posortowane oby dwoma algorytmami a winiki zostana wyswietlone"<<endl;
+    int n;
+    cout <<"\nPodaj pojemnosc tablicy: ";
+    cin >> n;
+
+    long int arr[n]; //arr_copy[n];
+    fill_array(arr, n);
+    //copy_array(arr, arr_copy, n);
+    insertionSort(arr, n);
+    //heapSort(arr_copy, n);
+    //printArray(arr, n);
+    return 0;
+}
+
 void read_theory()
 {
     fstream theory;
@@ -69,72 +149,4 @@ void read_theory()
     else{
         cout << "blad dostepu do pliku";
     }
-}
-
-int operation()
-{
-    bool check = true;
-    int user_choice;
-    cout << "Menu\nAby przejsc do wybranego punktu z wymienionych ponizej wpisz cyfre przypisana do opisu operacji.\n"
-         "1. Czesc teoretyczna\n2. Symulacje\n3. Autorzy\n4. Wyjscie\n\nTwoj wybor: ";
-    do{
-        cin >> user_choice;
-        switch(user_choice){
-            case 1:
-                read_theory();  check = false;break;
-            case 2:
-                sortowanie();   check = false;  break;
-            case 3:
-                cout << "3";    check = false;  break;
-            case 4:
-                return 0;   check = false;  break;
-            default:
-                    cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
-    }
-    }while( check == true);
-}
-
-int main(void)
-{
-    operation();
-    return 0;
-}
-
-int sortowanie()
-{
-    cout <<"1. Klasyczne sortowanie \n2. \n3. \n4. \n\nTwoj wybor: "<<endl;
-    int user_choice;
-    bool check = true;
-    do{
-        cin >> user_choice;
-        switch(user_choice){
-            case 1:
-                classic();  check = false;  break;
-            case 2:
-                cout << "2";    check = false;  break;
-            case 3:
-                cout << "3";    check = false;  break;
-            case 4:
-                return 0;   check = false;  break;
-            default:
-                    cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
-    }
-    }while( check == true);
-}
-
-int classic()
-{
-    cout << "W klasycznym sortowaniu zostan¹ wygenerowane tablice o roznych pojemnosciach liczbami losowymi\n"
-        "i posortowane oby dwoma algorytmami a winiki zostana wyswietlone"<<endl;
-    int n = 100;
-    int arr[n]; //arr_copy[n];
-    fill_array(arr, n);
-    //copy_array(arr, arr_copy, n);
-    cout <<"Tablica o wielkosci 100 elementow zostala wypelniona...\n"<<endl;
-    //insertionSort(arr, n);
-    //printArray(arr_copy, 100);
-    //heapSort(arr_copy, n);
-    cout <<"Posortowana tablica\n"<<endl;
-    //printArray(arr_copy, 100);
-    return 0;
 }
--- menu_functions.cpp
+++ menu_functions.cpp
@@ -3,5 +3,7 @@
 
 using namespace std;
 
-int sortowanie();
-int classic();
+int menu_sortowanie();
+int classic_sort();
+int menu();
+void read_theory();
