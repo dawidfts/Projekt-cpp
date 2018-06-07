48784d43e34dc06da7fbe56447603f41a7489f44
Kasiaak <35401505+Kasiaak@users.noreply.github.com>
2018-06-07 15:37:27 +0200

    fixed visual side
    now things which printed on screen are easier to read from code (f.ex each cout in separate lines, added gaps, etc)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 #include "menu_functions.cpp"
 
+#define HOW_MANY_ELEMENTS_TO_PRINT 30
+
 void insertionSort(long int arr[], int n)
 {
     std::clock_t start;
@@ -22,10 +24,11 @@ void insertionSort(long int arr[], int n)
         }
         arr[j+1] = key;
     }
+
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
     cout << "Parametry otrzymane dla sortowania przez wstawianie: \n" << endl;
     std::cout << "\nCzas operacji: " << duration << endl;
-    cout << "Wykonano " << counter << " operacji\n"<< endl;//
+    cout << "Wykonano " << counter << " operacji\n"<< endl;
 }
 
 void heapSort(long int arr_copy[], int n)
@@ -37,6 +40,7 @@ void heapSort(long int arr_copy[], int n)
 
     long long int counter_h = 0;
     int j,k,x;
+    cout << arr_copy[n] << endl;
     for(int i = 2; i <= n; i++){
         j = i; k = j / 2;
         x = arr_copy[i];
@@ -46,6 +50,7 @@ void heapSort(long int arr_copy[], int n)
         }
         arr_copy[j] = x;
     }
+
     duration_h = (std::clock() - start_h ) / (double) CLOCKS_PER_SEC;
     cout << "Parametry otrzymane dla sortowania przez kopcowanie: \n" << endl;
     std::cout << "\nCzas operacji: "<< duration_h << endl;
@@ -65,13 +70,18 @@ void copy_array(long int arr[], long int arr_copy[], int n)
 {
     for(int i = 0; i < n; i++){
         arr_copy[i] = arr[i];
+       /// cout << arr_copy[i] << endl;
     }
+    cout << endl;
 }
 
 void printArray(long int arr[], int n)
 {
    for (int i=0; i < n; i++)
-        cout << arr[i] << endl;
+   {
+        cout << arr[i] << " ";
+   }
+    cout <<  endl ;
 }
 
 int main(void)
@@ -84,19 +94,33 @@ int menu()
 {
     bool check = true;
     int user_choice;
-    cout << "\t\t\t\t| Menu |\n\nAby przejsc do wybranego punktu z wymienionych ponizej wpisz cyfre przypisana do opisu operacji.\n"
-         "\n1. Czesc teoretyczna\n2. Symulacje\n3. Autorzy\n4. Wyjscie\n\nTwoj wybor: ";
+    cout << "\t\t\t\t| Menu |\n";
+    cout << "Aby przejsc do wybranego punktu z wymienionych ponizej wpisz cyfre przypisana do opisu operacji.\n";
+    cout << "1. Czesc teoretyczna\n";
+    cout << "2. Symulacje\n";
+    cout << "3. Autorzy\n";
+    cout << "4. Wyjscie\n\n";
+    cout << "Twoj wybor: ";
     do{
         cin >> user_choice;
         switch(user_choice){
             case 1:
-                read_theory();  check = false;break;
+                read_theory();
+                check = false;
+                break;
+
             case 2:
-                menu_sortowanie();   check = false;  break;
+                menu_sortowanie();
+                check = false;
+                break;
             case 3:
-                cout << "3";    check = false;  break;
+                cout << "3";
+                check = false;
+                break;
             case 4:
-                return 0;   check = false;  break;
+                return 0;
+                check = false;
+                break;
             default:
                     cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
     }
@@ -107,32 +131,64 @@ int menu()
 int menu_sortowanie()
 {
     system("cls");
-    cout <<"\t\t\t\t| Menu |\n\n1. Klasyczne sortowanie \n2. \n3. \n4. \n\nTwoj wybor: ";
+    cout <<"\t\t\t\t| Menu |\n\n";
+    cout << "1. Klasyczne sortowanie \n";
+    cout << "2. \n";
+    cout << "3. \n";
+    cout << "4. \n\n";
+    cout << "Twoj wybor: ";
     int user_choice;
     bool check = true;
     do{
         cin >> user_choice;
         switch(user_choice){
             case 1:
-                classic_sort();  check = false;  break;
+                classic_sort();
+                check = false;
+                break;
             case 2:
-                cout << "2";    check = false;  break;
+                cout << "2";
+                check = false;
+                break;
             case 3:
-                cout << "3";    check = false;  break;
+                cout << "3";
+                check = false;
+                break;
             case 4:
-                return 0;   check = false;  break;
+                return 0;
+                check = false;
+                break;
             default:
                     cout << "Wprowadzono niepoprawna cyfre. Podaj jeszcze raz: ";
     }
     }while( check == true);
     return 0;
 }
-
+void print_before(int n, long int *arr)
+{
+    if(n <= HOW_MANY_ELEMENTS_TO_PRINT)
+    {
+        cout << "Zawartosc tablicy przed posortowaniem: " << endl;
+        cout << endl;
+        printArray(arr, n);
+        cout << endl;
+        cout << endl;
+    }
+}
+void print_after(int n, long int *arr)
+{
+    if(n <= HOW_MANY_ELEMENTS_TO_PRINT)
+    {
+        cout << "Zawartosc tablicy po posortowaniu: " << endl;
+        cout << endl;
+        printArray(arr, n);
+    }
+}
 int classic_sort()
 {
     system("cls");
-    cout << "W klasycznym sortowaniu zostana wygenerowane tablice o roznych pojemnosciach\n"
-        "liczbami losowymi i posortowane obydwoma algorytmami a wyniki zostana wyswietlone"<<endl;
+    cout << "W klasycznym sortowaniu zostana wygenerowane tablice o roznych pojemnosciach\n";
+    cout << "liczbami losowymi i posortowane obydwoma algorytmami a wyniki zostana wyswietlone" << endl;
     int n;
     cout <<"\nPodaj pojemnosc tablicy: ";
     cin >> n;
@@ -141,9 +197,10 @@ int classic_sort()
     long int arr[n], arr_copy[n];
     fill_array(arr, n);
     copy_array(arr, arr_copy, n);
+    //print_before(n, arr);
     insertionSort(arr, n);
     heapSort(arr_copy, n);
-    //printArray(arr, n);
+   // print_after(n, arr);
     return 0;
 }
 
